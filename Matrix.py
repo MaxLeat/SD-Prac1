e017279b6db5b2315370ae270636963e8c4c3faf
Ramon Donadeu <ramondonadeucaballero@gmail.com>
2020-04-01 20:06:36 +0200

    Treball ACabat
    Crec que funciona tot, pero hi ha vegades que hi ha algun error, crec que sera perque no controlem alguna dada

--- Matrix.py
+++ Matrix.py
@@ -2,6 +2,7 @@ import numpy as np
 import pywren_ibm_cloud as pywren
 import pickle
 import time
+import math
 from random import randint
 
 #ESPAI VARIABLES GLOBALS
@@ -14,113 +15,142 @@ colunnaStr="Columna_"
 
 def inicialitzacio(files, columnes, columnesB, ibm_cos):
 
-   #Columnes de a = files de b
-   #Creem unes matrius random i les omplim un altre cop (perque el random.ranndint no va bé)  
-    A =np.random.randint(10, size=(files, columnes))
-    B =np.random.randint(10, size=(columnes, columnesB))
-    C =np.random.randint(10, size=(files, columnesB))
+   # Columnes de a = files de b
+   # Creem unes matrius random i les omplim un altre cop (perque el random.ranndint no va bé)
+    A = np.random.randint(10, size=(files, columnes))
+    B = np.random.randint(10, size=(columnes, columnesB))
+    C = np.random.randint(10, size=(files, columnesB))
 
-    #S'omplen les matrius amb valors aleatoris de rang 10
+    # S'omplen les matrius amb valors aleatoris de rang 10
     for i in range(files):
         for j in range(columnes):
-            A[i,j]=randint(0,10)
+            A[i, j] = randint(0, 10)
 
     for i in range(columnes):
         for j in range(columnesB):
-            B[i,j]=randint(0,10)
- 
-    #Per cada fila de A faig un fitxer
-    for i in range(files):
-        fitxer=filaStr + str(i)
-        ibm_cos.put_object(Bucket='sdurv', Key=fitxer, Body = pickle.dumps(A[i], pickle.HIGHEST_PROTOCOL))
-    
-    #Per cada columna de B faig un fitxer
-    for i in range(columnesB):
-        fitxer=colunnaStr + str(i)
-        ibm_cos.put_object(Bucket='sdurv', Key=fitxer, Body = pickle.dumps(B[:,i], pickle.HIGHEST_PROTOCOL))
+            B[i, j] = randint(0, 10)  
 
-    #Pujo la matriu C per omplir-la al reduce
-    ibm_cos.put_object(Bucket='sdurv', Key='MatriuC.txt', Body = pickle.dumps(C, pickle.HIGHEST_PROTOCOL))
+    # Per cada fila de A faig un fitxer
+    for i in range(SubMA):
+        # Si la matriu no esta buida
+        if np.size(A[i*divisioFil:((i+1)*divisioFil)]) != 0:
+            fitxer = filaStr + str(i*divisioFil)
+            ibm_cos.put_object(Bucket=nom_cos, Key=fitxer,
+                               Body=pickle.dumps(A[i*divisioFil:((i+1)*divisioFil)], pickle.HIGHEST_PROTOCOL))
+
+    # Per cada columna de B faig un fitxer
+    for i in range(SubMB):
+        if np.size(B[:, i*divisioCol:((i+1)*divisioCol)]) != 0:
+            fitxer = colunnaStr + str(i*divisioCol)
+            ibm_cos.put_object(Bucket=nom_cos, Key=fitxer,
+                               Body=pickle.dumps(B[:, i*divisioCol:((i+1)*divisioCol)], pickle.HIGHEST_PROTOCOL))
+
+    # Pujo la matriu C per omplir-la al reduce
+    ibm_cos.put_object(Bucket=nom_cos, Key='MatriuC.txt',
+                       Body=pickle.dumps(C, pickle.HIGHEST_PROTOCOL))
+
 
 def matrix_mul(fitxers, ibm_cos):
 
-    resultats=[]
-    i=0
-    #Aqui tinc de dos en dos els noms dels fitxers
-    fit= fitxers.split()
+    resultats = []
+    i = 0
+    # Aqui tinc de dos en dos els noms dels fitxers
+    fit = fitxers.split()
     num_strings = len(fit)
 
     while i < num_strings:
-        #Descarrego els arxius que em fan falta
-        fitxerA=fit[i]
-        fitxerB=fit[i+1]
-        A_s=ibm_cos.get_object(Bucket='sdurv', Key=fitxerA)['Body'].read()
-        B_s=ibm_cos.get_object(Bucket='sdurv', Key=fitxerB)['Body'].read()
-        A=pickle.loads(A_s)
-        B=pickle.loads(B_s)
-        #Faig les operacions
-        C=A.dot(B)
+        # Descarrego els arxius que em fan falta
+        fitxerA = fit[i]
+        fitxerB = fit[i+1]
+        A_s = ibm_cos.get_object(Bucket=nom_cos, Key=fitxerA)['Body'].read()
+        B_s = ibm_cos.get_object(Bucket=nom_cos, Key=fitxerB)['Body'].read()
+        A = pickle.loads(A_s)
+        B = pickle.loads(B_s)
+        # Faig les operacions
+        C = A.dot(B)
         resultats.append(C)
-        i=i+2
+        i = i+2
 
     return resultats
 
+
 def reduce_function(results, ibm_cos):
+    x = 0
+    y = 0
+    # En aquesta funció el que hem de fer es recollir els resultats de l'execució anterior
+    C_s = ibm_cos.get_object(Bucket=nom_cos, Key='MatriuC.txt')['Body'].read()
+    C = pickle.loads(C_s)
+    # Recorrem els diferents workers
+    for w in range(len(results)):
+        # Recorrem les operacions de cad worker
+        for o in range(len(results[w])):
+            # Recorrem la coordenada i
+            for i in range(len(results[w][o])):
+                # Recorrem la cordenada j
+                for j in range(len(results[w][o][i])):
+                    C[i+x][j+y] = results[w][o][i][j]
 
-    #En aquesta funció el que hem de fer es recollir els resultats de l'execució anterior
-    C_s=ibm_cos.get_object(Bucket='sdurv', Key='MatriuC.txt')['Body'].read()
-    C=pickle.loads(C_s)
-    f_inici=0
-    c_inici=0
-    #Fem un recorregut per la llista de resultats i els posem a la matriu C
-    for i in range(len(results)):
-        for j in range(len(results[i])):
-            C[f_inici][c_inici] = results[i][j]
-            c_inici = c_inici + 1
-            if c_inici >= columnaB:
-                c_inici = 0
-                f_inici = f_inici +1
-
+            # Amb aixo controlem on coloquem els numeros a la matriu resultant ja que sino els posariem desordenats
+            y = divisioCol + y
+            if(y >= columna):
+                x = divisioFil + x
+                if(x >= divisioFil):
+                    y = 0
     return C
+
 
 if __name__ == '__main__':
     pw = pywren.ibm_cf_executor()
-    operacions=fila *columnaB
-
-    #Fem la comprovació de que els workers que s'han posat siguin acceptables
+    # Comprovem el valor de les variables referents a la creacio de submatrius
+    if divisioFil > (fila/workers):
+        divisioFil = math.ceil(fila/workers)
+    if divisioFil <= 0:
+        divisioFil = 1
+    if divisioCol > columna:
+        divisioCol = columna
+    if divisioCol <= 0:
+        divisioCol = 1
+    # Comprovem que les matrius son multiplicables:
+    # Calculem el numero de divisions que obtindrem
+    SubMA = math.ceil(fila/divisioFil)
+    SubMB = math.ceil(columna/divisioCol)
+    print(divisioFil)
+    operacions = SubMA * SubMB
+    print(operacions)
+    print (workers)
+    # Fem la comprovació de que els workers que s'han posat siguin acceptables
     if (workers <= operacions) and (workers > 0):
-        #D'aquesta manera tenim les operacions que ha de fer cada worker i les que falten
-        operacions_worker= operacions // workers
+        # D'aquesta manera tenim les operacions que ha de fer cada worker i les que falten
+        operacions_worker = operacions // workers
         resten = operacions - (operacions_worker * workers)
-        #Inicialitzem les matrius
-        pw.call_async(inicialitzacio, [fila,columna,columnaB])
 
-        iterdata=[]
-        f_inici=0;
-        c_inici=0;
-
-        #Creem les dades per passar al map_reduce
+        # Inicialitzem les matrius
+        pw.call_async(inicialitzacio, [fila, columna, columnaB])
+        iterdata = []
+        f_inici = 0
+        c_inici = 0
+        # Creem les dades per passar al map_reduce
         for i in range(workers):
             iterdata.append([])
-            iterdata[i]=""
-            #Miro si és l'últim per fer més operacions o no depenent de si hi ha resto
-            if (resten !=0) and (i == workers - 1):
-                operacions_worker=resten + operacions_worker
+            iterdata[i] = ""
+            # Miro si és l'últim per fer més operacions o no depenent de si hi ha resto
+            if (resten != 0) and (i == workers - 1):
+                operacions_worker = resten + operacions_worker
             for j in range(operacions_worker):
                 if f_inici < fila:
                     if c_inici >= columnaB:
-                        #si estem aqui vol dir que hem de posar columna 0 i aumentar la fila
-                        c_inici=0
-                        f_inici=f_inici+1
-                #Si es l'ultim ha de fer les que faltin no només les necessaries
-                nom_f=filaStr + str(f_inici)
-                nom_c=colunnaStr + str(c_inici)
-                iterdata[i]=iterdata[i] + str(nom_f) + " " + str(nom_c) + " "
-                c_inici = c_inici + 1 
-        
+                        # si estem aqui vol dir que hem de posar columna 0 i aumentar la fila
+                        c_inici = 0
+                        f_inici = f_inici+divisioFil
+                # Si es l'ultim ha de fer les que faltin no només les necessaries
+                nom_f = filaStr + str(f_inici)
+                nom_c = colunnaStr + str(c_inici)
+                iterdata[i] = iterdata[i] + str(nom_f) + " " + str(nom_c) + " "
+                c_inici = c_inici + divisioCol
+
         print(iterdata)
-            
-        #Fem la crida al map_reduce
+
+        # Fem la crida al map_reduce
         start_time = time.time()
         futures = pw.map_reduce(matrix_mul, iterdata, reduce_function)
         pw.wait(futures)
--- test_local.py
+++ test_local.py
@@ -3,37 +3,47 @@ import pywren_ibm_cloud as pywren
 import pickle
 from random import randint
 
-#Columnes de a = files de b
-files=3
-columnes=2
-#A=np.array([np.random.choice(5, 2),np.random.choice(5, 2),np.random.choice(5, 2)]) 
+# Columnes de a = files de b
+fila = 23
+columna = 23
+# Tamany maxim que tindra un fitxer que conte files de A (No pot ser major que columnes / workers)
+divisioFil = 3
+# Tamany maxim que tindra un fitxer que conte columnes de B (No pot ser major que el numero de Files)
+divisioCol = 3
+SubMA = 0  # Numero de submatrius A que tindrem, Estblim el tamany un cop haguem comprovat que la resta de valors son correctes
+SubMB = 0  # Numero de Submatrius B que tindrem, Estblim el tamany un cop haguem comprovat que la resta de valors son correctes
+filaStr = "Fila_"
+colunnaStr = "Columna_"
+#A=np.array([np.random.choice(5, 2),np.random.choice(5, 2),np.random.choice(5, 2)])
 #B=np.array([np.random.choice(5, 2),[randint(0,10),randint(0,10)]])
-#A=np.array([[1,2],[2,3],[3,4]]) 
-#B=np.array([[1,2],[7,4]])
+# A=np.array([[1,2],[2,3],[3,4]])
+# B=np.array([[1,2],[7,4]])
 
-#Inicialitzo les matrius buides
+# Inicialitzo les matrius buides
 #A = np.empty((files, columnes))
 #B = np.empty((columnes,files))
-A =np.random.randint(10, size=(files, columnes))
-B =np.random.randint(10, size=(columnes, files))
-for i in range(files):
-    for j in range(columnes):
-        A[i,j]=randint(0,10)
+SubMA = fila//divisioFil + ((fila/divisioFil)-(fila//divisioFil))
+SubMB = columna//divisioCol + 1
+A = np.random.randint(3, size=(fila, columna))
+B = np.random.randint(3, size=(columna, fila))
+for i in range(fila):
+    for j in range(columna):
+        A[i, j] = randint(0, 3)
 
-for i in range(columnes):
-    for j in range(files):
-        B[i,j]=randint(0,10)
+for i in range(columna):
+    for j in range(fila):
+        B[i, j] = randint(0, 3)
 
-nom_variable="fila_"
-i=1
-nom_variable=nom_variable + str(i)
+nom_variable = "fila_"
+i = 1
+nom_variable = nom_variable + str(i)
 print(nom_variable)
 
-#Omplo les matrius amb valors aleatoris
-#for i in range(files):
+# Omplo les matrius amb valors aleatoris
+# for i in range(files):
 #   A[i]=np.random.choice(10,columnes)
 
-#for j in range(columnes):
+# for j in range(columnes):
 #    B[j]=np.random.choice(10,files)
 
 print("Matriu A")
@@ -43,13 +53,32 @@ print("Matriu B")
 print(B)
 print()
 
-FA=A[0]
-CB=B[:,0]
+FA = A[0]
+CB = B[:, 0]
 print(FA)
 print(CB)
 
-
-#Fem la multiplicació
-C=FA.dot(CB)
+# Fem la multiplicació
+C = FA.dot(CB)
 print("Resultat")
 print(C)
+
+for i in range(SubMA):    
+    if np.size(A[i*divisioFil:((i+1)*divisioFil)])!=0:
+        fitxer = filaStr + str(i*divisioFil)
+        print (fitxer)  
+        uno = (A[i*divisioFil:((i+1)*divisioFil)]) 
+        print (uno)
+    print ("\n")
+    if np.size(B[:, i*divisioCol:((i+1)*divisioCol)])!=0:
+        fitxer = colunnaStr + str(i*divisioCol)
+        print (fitxer)
+        dos = (B[:, i*divisioCol:((i+1)*divisioCol)])
+        print (dos)
+    X=uno.dot(dos)
+    print ("Resultat :")
+    print (X)
+    print ("---------------------\n")
+
+print ("operacions = " + str(SubMB*SubMA))
+   
